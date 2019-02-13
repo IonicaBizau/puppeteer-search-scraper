6541607c68fbb1656f7b810378769bc7cc3bcd61
Ionică Bizău <bizauionica@gmail.com>
2019-02-13 15:57:03 +0100

    Make it generic

--- example/index.js
+++ example/index.js
@@ -1,26 +1,63 @@
 "use strict";
 
-const puppeteerGoogleScraper = require("../lib");
+const SearchScraper = require("../lib");
 
-puppeteerGoogleScraper("who killed kenedy", {
-    limit: 20
-  , headless: false
-}).then(d => {
-    console.log(d)
-    // [ { title: 'John F. Kennedy assassination conspiracy theories - Wikipedia',
-    //     url: 'https://en.wikipedia.org/wiki/John_F._Kennedy_assassination_conspiracy_theories' },
-    //   { title: 'Assassination of John F. Kennedy - Wikipedia',
-    //     url: 'https://en.wikipedia.org/wiki/Assassination_of_John_F._Kennedy' },
-    //   { title: 'John F. Kennedy assassinated - HISTORY',
-    //     url: 'https://www.history.com/this-day-in-history/john-f-kennedy-assassinated' },
-    //   { title: 'November 22, 1963: Death of the President | JFK Library',
-    //     url: 'https://www.jfklibrary.org/learn/about-jfk/jfk-in-history/november-22-1963-death-of-the-president' },
-    //   { title: 'Who killed JFK? | HowStuffWorks',
-    //     url: 'https://history.howstuffworks.com/history-vs-myth/who-killed-jfk.htm' },
-    //   { title: 'The Man Who Killed Kennedy: The Case Against LBJ: Amazon.es ...',
-    //     url: 'https://www.amazon.es/Man-Who-Killed-Kennedy-Against/dp/1629144894' },
-    //   ...
-    // ]
+const headless = false
+SearchScraper.configure([
+    {
+        name: "GoogleCom"
+      , debugDir: "public/GoogleCom"
+      , searchUrl: "http://google.com/webhp?num=100"
+      , limit: 100
+      , selectors: SearchScraper.Selectors.GOOGLE
+      , headless
+    }
+  , {
+        name: "GoogleCoUk"
+      , debugDir: "public/GoogleCoUk"
+      , searchUrl: "http://google.co.uk/webhp?num=100"
+      , limit: 100
+      , selectors: SearchScraper.Selectors.GOOGLE
+      , headless
+    }
+  , {
+        name: "GoogleCoAu"
+      , debugDir: "public/GoogleComAu"
+      , searchUrl: "http://google.com.au/webhp?num=100"
+      , limit: 100
+      , selectors: SearchScraper.Selectors.GOOGLE
+      , headless
+    }
+  , {
+        name: "Bing"
+      , debugDir: "public/Bing"
+      , searchUrl: "http://bing.com/"
+      , limit: 100
+      , selectors: SearchScraper.Selectors.BING
+      , headless
+    }
+])
+
+const QUERY = "who killed kennedy"
+console.log(">>>> Google.com")
+SearchScraper.search(QUERY, { engine: "GoogleCom" }).then(res => {
+    console.log(res)
+    console.log(">>>> Google.com")
+    return SearchScraper.search(QUERY, { engine: "GoogleCom" })
+}).then(res => {
+    console.log(res)
+    console.log(">>>> Google.co.uk")
+    return SearchScraper.search(QUERY, { engine: "GoogleCoUk" })
+}).then(res => {
+    console.log(res)
+    console.log(">>>> Google.com.au")
+    return SearchScraper.search(QUERY, { engine: "GoogleCoAu" })
+}).then(res => {
+    console.log(res)
+    console.log(">>>> Bing.com")
+    return SearchScraper.search(QUERY, { engine: "Bing" })
+}).then(res => {
+    console.log(res)
 }).catch(err => {
     console.error(err)
 })

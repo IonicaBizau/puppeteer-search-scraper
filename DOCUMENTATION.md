d5d423989f09f0277db177698022b8f213376f54
Ionică Bizău <bizauionica@gmail.com>
2019-02-13 14:34:49 +0100

    Add the other files

---
+++ DOCUMENTATION.md
@@ -0,0 +1,15 @@
+## Documentation
+
+You can see below the API reference of this module.
+
+### `puppeteerGoogleScraper(a, b)`
+Scrape Google using Puppeteer
+
+#### Params
+
+- **Number** `a`: Param descrpition.
+- **Number** `b`: Param descrpition.
+
+#### Return
+- **Number** Return description.
+
--- example/index.js
+++ example/index.js
@@ -2,4 +2,16 @@
 
 const puppeteerGoogleScraper = require("../lib");
 
-console.log(puppeteerGoogleScraper());
+puppeteerGoogleScraper("who killed kenedy", {
+    limit: 20
+  , headless: false
+}).then(d => {
+    console.log(d)
+    // [ { title: 'Why Node.js is cool – Andrew Winstead – Medium',
+    //     url: 'https://medium.com/@awinste/why-node-js-is-cool-a61710eec906' },
+    //   { title: 'Why Node.js Is Totally Awesome - Chetan Surpur',
+    //     url: 'http://chetansurpur.com/blog/2010/10/why-node-js-is-totally-awesome.html' },
+    //   ...  ]
+}).catch(err => {
+    console.error(err)
+})
